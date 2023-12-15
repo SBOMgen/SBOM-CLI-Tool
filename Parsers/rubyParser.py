07aa9b2fd39c5054c8a63a5a1c287c71c1eb6f59
RISHIKESHk07 <117551967+RISHIKESHk07@users.noreply.github.com>
2023-12-15 18:35:04 +0530

    Update rubyParser.py
    changed some parts for "dependencies and made it withou return functions

--- Parsers/rubyParser.py
+++ Parsers/rubyParser.py
@@ -72,8 +72,23 @@ import re
 import json
 import datetime
 import glob
+global_dict =[]
+def global_dict_list(content):
+    current_gem = None
+    for line in content.split('\n'):
+        match_gem = re.match(r'\s{4}([\w-]+) \(([\d.]+)\)\s*$', line)
+        if match_gem:
+           current_gem = match_gem.group(1)
+           component3={
+                "name": current_gem,
+                "version": match_gem.group(2)
+            }
+           global_dict.append(component3)
+             
+
 
 def parse_gemfile_lock(content, sbom, lock_file_path):
+    global_dict_list(content)
     current_gem = None
     current_dependencies = None
     added_dependencies = set()
@@ -110,12 +125,12 @@ def parse_gemfile_lock(content, sbom, lock_file_path):
                 }
             }
             component2={
-                "name": current_gem,
-                "version": match_gem.group(2),
                 "bom-ref": f"pkg:ruby/{current_gem}@{match_gem.group(2)}",
                 "dependsOn":[],
             }
+            
             sbom["components"].append(component)
+            
             current_dependencies = []
 
         if match_dep:
@@ -142,7 +157,18 @@ def parse_gemfile_lock(content, sbom, lock_file_path):
         if current_dependencies and line.strip().endswith(')'):
             if current_dependencies:
                 component["dependsOn"] = current_dependencies
-                component2["dependsOn"]=component["dependsOn"]
+               
+                dict=[]
+                for i in component["dependsOn"]:
+                     
+                     
+                     for j in global_dict:
+                         if(j["name"] == i["name"]):
+                             i["version"] = j["version"]
+                     k= "pkg:ruby/"+i["name"]+"@"+i["version"]        
+                     dict.append(k)
+                component2["dependsOn"] =dict    
+
                 if component["bom-ref"] not in added_dependencies:
 
                     sbom["dependencies"].append(component2)
@@ -151,8 +177,8 @@ def parse_gemfile_lock(content, sbom, lock_file_path):
     
 
 
-
-    return sbom
+    
+    
 
 def rubyparser(path, sbom):
    for p in glob.glob(os.path.join(path, "**", 'Gemfile.lock'), recursive=True):
@@ -161,5 +187,5 @@ def rubyparser(path, sbom):
 
        sbom = parse_gemfile_lock(gemfile_lock_content, sbom, p)
 
-   return sbom
+   
 
